0f5f5f2944bbed616c364bd991b22803814c63bf
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2018-11-08 14:50:04 +0100

    no message

--- 02 SOLID - OCP Pattern.playground/Contents.swift
+++ 02 SOLID - OCP Pattern.playground/Contents.swift
@@ -9,4 +9,53 @@
 import Foundation
 import UIKit
 
+enum Team {
+    case Australia
+    case India
+    case Croatia
+    case USA
+}
 
+enum Role {
+    case batsman
+    case bowler
+    case allrounder
+}
+
+class Cricketer {
+    var name: String?
+    var team: Team
+    var role: Role
+    init(_ name: String, _ team: Team, _ role: Role) {
+        self.name = name
+        self.team = team
+        self.role = role
+    }
+}
+
+class CricketerFilter {
+    func filterByTeam(_ criketers: [Cricketer], _ team: Team) -> [Cricketer] {
+        var filteredResult = [Cricketer]()
+        
+        for item in criketers {
+            if item.team == team{
+                filteredResult.append(item)
+            }
+        }
+        return filteredResult
+    }
+}
+
+func main(){
+    let sinisa = Cricketer("Sinisa", .Croatia, .allrounder)
+    let jordan = Cricketer("Jordan", .USA, .batsman)
+    let mihael = Cricketer("Mihael", .India, .batsman)
+    let drazen = Cricketer("Drazen", .Australia, .bowler)
+    let pero = Cricketer("Pero", .Australia, .batsman)
+    let tonci = Cricketer("Antonio", .Croatia, .batsman)
+    let luka = Cricketer("Luka", .India, .allrounder)
+    
+    let cricketers = [sinisa, jordan, mihael, drazen, pero, tonci, luka]
+    
+    
+}
