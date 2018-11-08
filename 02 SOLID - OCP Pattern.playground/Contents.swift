8ae197d3d6fb54efea51cc936f82f132150e6d34
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2018-11-08 09:39:10 +0100

    Open closed principle

---
+++ 02 SOLID - OCP Pattern.playground/Contents.swift
@@ -0,0 +1,12 @@
+// Open closed principle (OCP)
+// one should be able to extend a class behaviour without modofiying it
+// Any class followong OCP should fulfill two citteria
+// 1) Open for extension. This nesures that the class behaviour cna be extended In real world scenario requirements keep changing
+// 2) Closed for modification
+
+// Example
+// 
+import Foundation
+import UIKit
+
+
